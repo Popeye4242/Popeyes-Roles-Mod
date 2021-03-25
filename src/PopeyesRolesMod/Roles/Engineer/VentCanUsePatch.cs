04fe2513185335072495ecb60a4f8ccf315a7e41
Popeye4242 <35311793+Popeye4242@users.noreply.github.com>
2021-03-25 08:31:18 +0100

    Disabled Morphling Vents

--- src/PopeyesRolesMod/Roles/Engineer/VentCanUsePatch.cs
+++ src/PopeyesRolesMod/Roles/Engineer/VentCanUsePatch.cs
@@ -14,7 +14,7 @@ namespace PopeyesRolesMod.Roles.Engineer
 
             canUse = false;
             couldUse = false;
-            if (pc.HasPlayerRole(Role.Engineer) || pc.IsImpostor)
+            if (pc.HasPlayerRole(Role.Engineer) || (pc.IsImpostor && !pc.HasPlayerRole(Role.Morphling)))
             {
                 var vent = __instance;
                 float num = float.MaxValue;
